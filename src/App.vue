a2d4cfaefa058028b97d81b3a61ee7fa017fb087
Oyxing <897346175@qq.com>
2017-09-18 17:42:36 +0800

    vuex-exercises

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+   <div id="app">
+			<product-list-one></product-list-one>
+			<product-list-two></product-list-two>
+   </div>
+</template>
+
+<script>
+import ProdectListOne from '@/components/ProductListOne'
+import ProdectListTwo from '@/components/ProductListTwo'
+
+export default {
+  name:'app',
+  components:{
+  	'product-list-one':ProdectListOne,
+		'product-list-two':ProdectListTwo
+  },
+  data(){
+  	return {
+		
+	}
+
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/ProductListOne.vue
@@ -0,0 +1,68 @@
+
+<template>
+   <div id="product-list-one">
+   	<h2>Prouct List One</h2>
+			<ul>
+				<li v-for="product in saleProducts">
+					<span class="name">{{product.name}}</span>
+					<span class="price">{{product.price}} </span>
+				</li>
+			</ul>
+			<button @click="reducePrice(4)">降价</button>
+   </div>
+</template>
+
+<script>
+import {mapGetters} from 'Vuex'
+import {mapActions} from 'Vuex'
+export default {
+	computed:{
+		products(){
+			return this.$store.state.products
+		},
+		// vuex 内部方程
+		...mapGetters([
+			"saleProducts"
+		])
+		// 调用的方法 来return 出数据
+		// saleProducts(){
+		// 	return this.$store.getters.saleProducts
+		// }
+	},
+		
+	},
+	methods:{
+		reducePrice(amount){
+			this.$store.dispatch('reducePrice',amount)
+		},
+		...mapActions([
+			"reducePrice"
+		])
+	}
+}
+</script>
+
+<style scoped>
+#product-list-one{
+    background: #FFF8B1;
+    box-shadow: 1px 2px 3px rgba(0,0,0,0.2);
+    margin-bottom: 30px;
+    padding: 10px 20px;
+}
+#product-list-one ul{
+    padding: 0;
+}
+#product-list-one li{
+    display: inline-block;
+    margin-right: 10px;
+    margin-top: 10px;
+    padding: 20px;
+    background: rgba(255,255,255,0.7);
+}
+.price{
+    font-weight: bold;
+    color: #E8800C;
+}
+
+
+</style>
